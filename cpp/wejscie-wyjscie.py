0bcc91bf4f15637948ffcccfb0e87bea025f3b06
Bananek96 <Banasiak_Jakub@wp.pl>
2018-09-11 14:18:56 +0200

    cpp

---
+++ cpp/wejscie-wyjscie.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  wejscie-wyjscie.py
+
+def main(args):
+    a = input("Podaj pierwsza liczbe: ")
+    print(a)
+    
+    b = input("Podaj druga liczbe: ")
+    print(b)
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
