39a46d5d32aea932984259642b3d72d6e01b0b76
Bananek96 <Banasiak_Jakub@wp.pl>
2018-10-09 14:18:56 +0200

    liczby

---
+++ python/liczby .py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  liczby.py
+
+def liczby2()
+    """
+    Drukuje wszystkie dwucyfrowe liczby o niepowtarzajacych sie cyfrach
+    """
+    pass
+    
+def liczby3()
+    """
+    Drukuje wszystkie trzycyfrowe liczby o niepowtarzajacych sie cyfrach
+    """
+    pass
+            
+def main(args):
+    print(liczby2(10, 99))
+    print(liczby3(100, 999))
+    return 0
+    
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
