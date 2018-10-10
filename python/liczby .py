15a7006a6e946199fa63e19b0ab4d887371846a4
Bananek96 <Banasiak_Jakub@wp.pl>
2018-10-10 07:17:05 +0200

    poprawki

--- python/liczby .py
+++ python/liczby .py
@@ -6,26 +6,34 @@
 def liczby2(a=10, b=99):
     
     """
-    Program drukuje wszystkie liczby dwucyfrowe o niepowtarzających się
-    cyfrach oraz zwraca ilość takich liczb
+    Drukuje wszystkie liczby 2-cyfrowe o niepowtarzających się
+    cyfrach i ich ilość
     """
     for i in range(a, b):
-        for j in range(a, b):
-            
+        if (i % 11) != 0:
+            print(i)
+        print()
+			          
         
     
 def liczby3(a=100, b=999):
     
     """
-    Program drukuje wszystkie liczby trzycyfrowe o niepowtarzających się
-    cyfrach oraz zwraca ilość takich liczb
+    Drukuje wszystkie liczby 3-cyfrowe o niepowtarzających się
+    cyfrach i ich ilość
     """
+    for j in range(a, b):
+        if (j % 11) != 0:
+            print(j)
+        print()
+
 def main(args):
     
-    print(liczby2)
-    print(liczby3)
+    print(liczby2())
+    print(liczby3())
     
     return 0
+
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
