229d581558f833c35b4ca2ae8a80df994bf7d64e
Bananek96 <Banasiak_Jakub@wp.pl>
2018-10-23 14:18:12 +0200

    cpp euklidesy

--- Klasa 2/cpp/nwd_euklides.cpp
+++ Klasa 2/cpp/nwd_euklides.cpp
@@ -6,6 +6,34 @@
 #include <iostream>
 using namespace std;
  
+int nwd_klasyczny(int a, int b)
+{   
+    int licznik = 0;
+    while (a != b)
+    {   
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+        licznik++;
+    }
+    cout << "Powtórzeń klasyczny: " << licznik << endl;
+    return a;
+}
+
+int nwd_optymalny(int a, int b)
+{   
+    int licznik = 0;
+    while (a > 0)
+    {
+            a = a % b;
+            b = b - a;
+            licznik++;
+    }
+    cout << "Powtórzeń optymalny: " << licznik << endl;
+    return b;
+}
+
 int main()
 {
     int a,b;
@@ -13,15 +41,9 @@ int main()
         cin >> a;
         cout << "Podaj b: ";
         cin >> b;
-        cout << "NWD(" << a << " i " << b << ") = ";
-    while (a != b)
-    {   
-        if (a > b)
-            a = a - b;
-        else
-            b = b - a;
-    }
-    cout << a << endl;
-    
+        cout << nwd_optymalny(a, b) << endl;
+        cout << nwd_klasyczny(a, b);
+   
     return 0;
 }
+
