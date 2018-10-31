f66ed2b30aeb67c5192c8f406b628906952deeb0
Bananek96 <Banasiak_Jakub@wp.pl>
2018-10-31 11:02:07 +0100

    znaki

--- klasa 2/cpp/znaki.cpp
+++ klasa 2/cpp/znaki.cpp
@@ -10,16 +10,69 @@ using namespace std;
 void licz_znaki(char tab[])
 {
     int i = 0;
-    int biale = 0;
+    int biale, inter, licz;
+    biale = inter = licz = 0;
     while (tab[i] != '\0')
     {
-        if (tab[i] == ' ' || tab[i] == '\t')
-            biale++;
-        else
-            cout << tab[i] << ' ';
+        //~ if (tab[i] == ' ' || tab[i] == '\t')
+            //~ biale++;
+        //~ else
+            //~ cout << tab[i] << ' ';
+        switch (tab[i])
+        {
+            case ' ':
+                biale++;
+                // inne;
+            break;
+            case '\t':
+                biale++;
+                // inne;
+            break;
+            case ',':
+                inter++;
+                // inne;
+            break;
+            case '.':
+                inter++;
+                // inne;
+            break;
+            default:
+                licz++;
+                // inne;
+            break;
+        }
         i++;  // zwiększanie indeksu
     }
     cout << "\nZnaków białych: " << biale << endl;
+    cout << "\nZnaków interpunkcyjnych: " << inter << endl;
+    cout << "\nReszta: " << licz << endl;
+}
+
+void ascii(char tab[])
+{
+    int i = 0;
+    while (tab[i] != '\0')
+    {
+        cout << (int)tab[i] << ' ';
+        i++;
+    }
+}
+
+// A-Z ascii 65-90
+// a-z ascii 97-122
+void zamiana(char tab[])
+{   
+    int i = 0;
+    while (tab[i] != '\0')
+    {   
+        (int)tab[i] = x;
+        if ( 65 <= x and x <= 90 )
+        {
+            x = x + 32;
+            cout << (char)x;      
+        }
+        i++;
+    }
 }
 
 int main(int argc, char **argv)
@@ -28,8 +81,11 @@ int main(int argc, char **argv)
     char znaki[rozmiar];  // deklaracja tablicy znakowej
     cout << "Jak się nazywasz: ";
     cin.getline(znaki, rozmiar);
-	cout << "Siema " << znaki << endl;
-    licz_znaki(znaki);
+	cout << "Spierdalaj " << znaki << endl;
+    //~ licz_znaki(znaki);
+    ascii(znaki);
+    zamiana(znaki);
+    
     
 	return 0;
 }
