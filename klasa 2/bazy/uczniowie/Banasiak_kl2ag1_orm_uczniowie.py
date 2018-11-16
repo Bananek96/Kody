609c883161ffe3edda9edb76e9277f4460c1fea1
Bananek96 <Banasiak_Jakub@wp.pl>
2018-11-16 08:18:40 +0100

    peewee

--- klasa 2/bazy/uczniowie/Banasiak_kl2ag1_orm_uczniowie.py
+++ klasa 2/bazy/uczniowie/Banasiak_kl2ag1_orm_uczniowie.py
@@ -14,29 +14,39 @@ class BazaModel(Model):
 
 class Klasa(BazaModel):
     
-    nazwa = CharField(null=False)
+    klasa = CharField(null=False)
     roknaboru = IntegerField(default=0)
     rokmatury = IntegerField(default=0)
-        
+    
 class Uczen(BazaModel):
     
     imie = CharField(null=False)
     nazwisko = CharField(null=False)
     plec = BooleanField()
     klasa = ForeignKeyField(Klasa, related_name='uczniowie')
-
-class Wynik(BazaModel):
-    
     egzhum = FloatField(default=0)
     egzmat = FloatField(default=0)
     egzjez = FloatField(default=0)
-    uczen = ForeignKeyField(Uczen, related_name='wyniki')
+        
+class Przedmiot(BazaModel):
+    
+    przedmiot = CharField(null=False)
+    imienaucz = CharField(null=False)
+    nazwisko_naucz = CharField(null=False)
+    plecnaucz = BooleanField()
+    
+class Ocena(BazaModel):
+    
+    datad = DateTimeField()
+    uczen = ForeignKeyField(Uczen, related_name='oceny')
+    przedmiot = ForeignKeyField(Przedmiot, related_name='oceny')
+    ocena = DecimalField(null=False)
 
 def main(args):
     if os.path.exists(baza_plik):
         os.remove(baza_plik)
     baza.connect() # połączenie z bazą
-    baza.create_tables([Klasa, Uczen, Wynik])
+    baza.create_tables([Uczen, Klasa, Przedmiot, Ocena])
     return 0
 
 if __name__ == '__main__':
