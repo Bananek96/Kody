f929f1ac483d656c12742fb31ec833ea71033372
Bananek96 <Banasiak_Jakub@wp.pl>
2018-11-30 07:14:44 +0100

    NWM

--- klasa 2/cpp/fibonacci.cpp
+++ klasa 2/cpp/fibonacci.cpp
@@ -6,6 +6,13 @@
 #include <iostream>
 using namespace std;
 
+float fibonacci_re(int n)
+{   
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci_re(n-1) + fibonacci_re(n-2);
+}
+
 int fibonacci_it(int n)
 {   
     int a = 0; // n-2
@@ -27,8 +34,18 @@ int main(int argc, char **argv)
 	int n;
     cout << "Podaj numer wyrazu ciągu: ";
     cin >> n;
-    cout << "Ciąg Fibonacci'ego do wyrazu " << n << " :" << endl;
-    cout << fibonacci_it(n);
+    cout << "Ciąg Fibonacci'ego ITERACYJNIE " << n << " :" << fibonacci_it(n) << endl;
+    cout << "Ciąg Fibonacci'ego REKURENCYJNIE " << n << " :" << fibonacci_re(n) << endl;
+    
+    for (int i = 0; i <= n; i++)
+    {
+        cout << fibonacci_re(i) << " ";
+        if (i < 2) continue;
+        else
+        {
+            cout << (float)fibonacii_it(i) / (float)fibonacci_it(i - 1) << endl;
+        }
+    }
     
 	return 0;
 }
