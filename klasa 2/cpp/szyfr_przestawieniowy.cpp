4c44ad20dba94f203c0b0d362a4b0d22d17962b2
Bananek96 <Banasiak_Jakub@wp.pl>
2019-02-20 11:32:49 +0100

    Szyfr Cezara

--- klasa 2/cpp/szyfr_przestawieniowy.cpp
+++ klasa 2/cpp/szyfr_przestawieniowy.cpp
@@ -1,53 +1,57 @@
+/*
+ * szyfr_cezara.cpp
+ */
+
 #include <iostream>
-#include <stdio.h>
 #include <string.h>
 
 using namespace std;
 
 #define MAKS 100
 
-void szyfruj(char tekst[], int klucz){
-    klucz = klucz % 26;
-    int kod = 0;
-    int ilosc = strlen(tekst);
-    
+void deszyfruj(char tb[], int k)
+{
+    ;
+}
+
+void szyfruj(char tb[], int k)
+{
     for(int i = 0; i < ilosc; i++)
     {
+        kod = (int)tekst[i] + klucz;
         kod = (int)tekst[i];
         if (tekst[i] == ' ')
-        {
-            ;
-        }
+            kod -= klucz;
+        else if(kod > 122)
+            kod -= 26;
         else if (kod < 91)
         {
             kod += klucz;
             if (kod > 90) kod -= 26;
-        }
-        else
+        } 
+        else 
         {
             kod += klucz;
-            if (kod > 122) kod -=26;
+            if (kod > 122) kod =-26;
         }
-        
+
         tekst[i] = char(kod);
         cout << tekst[i];
     }
-
 }
 
 
 int main(int argc, char **argv)
 {
     char tekst[MAKS];
-    int klucz = 0;
-    
+	int klucz = 0;
+
     cout << "Podaj tekst: " << endl;
     cin.getline(tekst, MAKS);
     
-    cout << "Podaj klucz: ";
+    cout << "Podaj klucz: " << endl;
     cin >> klucz;
-    
-    
+
     szyfruj(tekst, klucz);
-    return 0;
+	return 0;
 }
