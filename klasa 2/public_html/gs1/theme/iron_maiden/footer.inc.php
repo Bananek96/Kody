1ec42a171707ddf824fe11f5e7390570f46dbd83
Bananek96 <Banasiak_Jakub@wp.pl>
2019-05-24 08:45:13 +0200

    szablon dodaj

---
+++ klasa 2/public_html/gs1/theme/iron_maiden/footer.inc.php
@@ -0,0 +1,9 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); } ?>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <?php get_footer(); ?>
+      <p class="lead">&copy;Bananek</p>
+    </div>
+  </div>
+</div>
